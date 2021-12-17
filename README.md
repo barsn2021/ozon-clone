2e15b9ec96ce147147f69956d61af9844b2939c8
Andrey <maoltin@gmail.com>
2021-12-17 11:42:43 +0300

    1st lession with extras complete

---
+++ README.md
@@ -0,0 +1,10 @@
+## Dev:
+
+### Browser-sync: LiveServer alternative (yarn global add browser-sync)
+```bash
+yarn start
+```
+### Webpack rebuilding on save
+```bash
+yarn watch
+```
---
+++ src/modules/search.js
@@ -0,0 +1,12 @@
+const search = () => {
+  
+  const searchBtn = document.querySelector(".search .search-btn button");
+  
+  searchBtn.addEventListener("click", () => {
+    let searchValue = document.querySelector(".search-wrapper_input").value;
+    console.log(`input.value = ${searchValue}`);
+  })
+  console.log("second");
+};
+
+export default search;
