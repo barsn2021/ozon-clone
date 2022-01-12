fb83c7558fa338926d4062bf91ea09b4cec8c919
Nadezhda <barsn2021@gmail.com>
2022-01-12 06:14:30 +0500

    first commit

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,6 +1,6 @@
 const getData = () => {
   return fetch(
-    `https://test-ed653-default-rtdb.europe-west1.firebasedatabase.app/goods.json?`
+    `https://ozoncp-default-rtdb.firebaseio.com/goods.json?`
   ).then((response) => {
     return response.json();
   });
